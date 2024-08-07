10df151daeb2cf25ca1e17d297f5446fe6a8fe26
chengxing <zcx5613@gmail.com>
2024-08-07 20:16:29 +0900

    fixed error

--- include/cudaUtil.cuh
+++ include/cudaUtil.cuh
@@ -5,23 +5,23 @@
 namespace dev {
 template<typename T>
 inline void H2D(T *dev, const T *host, const size_t size) {
-    cudaMemcpy(dev, host, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev, host, size * sizeof(T), cudaMemcpyHostToDevice);
 }
 
 template<typename T>
 inline void D2H(T *host, const T *dev, const size_t size) {
-    cudaMemcpy(host, dev, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(host, dev, size * sizeof(T), cudaMemcpyDeviceToHost);
 }
 
 template<typename T>
 inline void H2D(T *dev, const std::vector<T> &host) {
-    cudaMemcpy(dev, host.data(), host.size(), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev, host.data(), host.size() * sizeof(T), cudaMemcpyHostToDevice);
 }
 
 template<typename T>
 inline void D2H(std::vector<T> &host, const T *dev, const size_t size) {
     host.clear();
     host.resize(size);
-    cudaMemcpy(host.data(), dev, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(host.data(), dev, size * sizeof(T), cudaMemcpyDeviceToHost);
 }
 } // namespace dev
