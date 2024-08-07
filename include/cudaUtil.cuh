783d0a753e4e10958be07ebf40358c528d4217b0
chengxing <zcx5613@gmail.com>
2024-08-07 17:52:05 +0900

    Add function

--- include/cudaUtil.cuh
+++ include/cudaUtil.cuh
@@ -5,11 +5,23 @@
 namespace dev {
 template<typename T>
 inline void H2D(T *dev, const T *host, const size_t size) {
-    cudaMemcpy(dev,host,size,cudaMemcpyHostToDevice);
+    cudaMemcpy(dev, host, size, cudaMemcpyHostToDevice);
 }
 
 template<typename T>
 inline void D2H(T *host, const T *dev, const size_t size) {
-    cudaMemcpy(host,dev,size,cudaMemcpyDeviceToHost);
+    cudaMemcpy(host, dev, size, cudaMemcpyDeviceToHost);
+}
+
+template<typename T>
+inline void H2D(T *dev, const std::vector<T> &host) {
+    cudaMemcpy(dev, host.data(), host.size(), cudaMemcpyHostToDevice);
+}
+
+template<typename T>
+inline void D2H(std::vector<T> &host, const T *dev, const size_t size) {
+    host.clear();
+    host.resize(size);
+    cudaMemcpy(host.data(), dev, size, cudaMemcpyDeviceToHost);
 }
 } // namespace dev
