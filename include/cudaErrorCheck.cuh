65d734dbcb9e03263123fbbe1e831bda7176d5a7
chengxing <zcx5613@gmail.com>
2024-08-08 17:25:09 +0900

    Update

--- include/cudaErrorCheck.cuh
+++ include/cudaErrorCheck.cuh
@@ -7,29 +7,29 @@
 
 // Define some error checking macros
 
-#define cudaErrCheck(stat) { cudaCheck::cudaErrCheck_((stat), __FILE__, __LINE__); }
-namespace cudaCheck{
+#define cudaErrCheck(stat) { cudaErrorCheck::cudaErrCheck_((stat), __FILE__, __LINE__); }
+namespace cudaErrorCheck{
 inline void cudaErrCheck_(cudaError_t stat, const char *file, int line) {
     if (stat != cudaSuccess) {
         fprintf(stderr, "CUDA Error: %s %s %d\n", cudaGetErrorString(stat), file, line);
     }
 }
-} // namespace cudaCheck
+} // namespace cudaErrorCheck
 
 #define cublasErrCheck(stat) { cudaCheck::cublasErrCheck_((stat), __FILE__, __LINE__); }
-namespace cudaCheck{
+namespace cudaErrorCheck{
 inline void cublasErrCheck_(cublasStatus_t stat, const char *file, int line) {
     if (stat != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "cuBLAS Error: %d %s %d\n", stat, file, line);
     }
 }
-} // namespace cudaCheck
+} // namespace cudaErrorCheck
 
 #define curandErrCheck(stat) { cudaCheck::curandErrCheck_((stat), __FILE__, __LINE__); }
-namespace cudaCheck{
+namespace cudaErrorCheck{
 inline void curandErrCheck_(curandStatus_t stat, const char *file, int line) {
     if (stat != CURAND_STATUS_SUCCESS) {
         fprintf(stderr, "cuRand Error: %d %s %d\n", stat, file, line);
     }
 }
-} // namespace cudaCheck
+} // namespace cudaErrorCheck
