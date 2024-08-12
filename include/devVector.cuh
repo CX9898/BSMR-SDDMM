38249e4a50482236a93b3ded7a5ab11bfac8c4b6
chengxing <zcx5613@gmail.com>
2024-08-12 16:39:32 +0900

    Update

--- include/devVector.cuh
+++ include/devVector.cuh
@@ -5,39 +5,39 @@ namespace dev {
 template<typename T>
 class vector {
  public:
-  vector() : size_(0), data_(nullptr) {};
+  vector() : _size(0), _data(nullptr) {};
   vector(int size);
   ~vector();
 
   inline __host__ __device__ size_t size() {
-      return size_;
+      return _size;
   }
   inline __host__ __device__ const T *data() const {
-      return data_;
+      return _data;
   }
   inline __host__ __device__ T *data() {
-      return data_;
+      return _data;
   }
   inline __device__ const T &operator[](size_t idx) const {
-      return data_[idx];
+      return _data[idx];
   }
   inline __device__ T &operator[](size_t idx) {
-      return data_[idx];
+      return _data[idx];
   }
 
  private:
-  size_t size_;
-  T *data_;
+  size_t _size;
+  T *_data;
 };
 
 template<typename T>
 inline vector<T>::vector(const int size) : vector() {
-    size_ = size;
-    cudaMalloc(reinterpret_cast<void **>(data_), size);
+    _size = size;
+    cudaMalloc(reinterpret_cast<void **>(_data), size * sizeof(T));
 }
 
 template<typename T>
 inline vector<T>::~vector() {
-    if (data_) { cudaFree(data_); }
+    if (_data) { cudaFree(_data); }
 }
 } // namespace dev
