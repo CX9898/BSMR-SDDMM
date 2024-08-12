13488b15be04ee609cd5b62f58bf7be2a792409b
chengxing <zcx5613@gmail.com>
2024-08-12 16:47:20 +0900

    Update

--- include/devVector.cuh
+++ include/devVector.cuh
@@ -6,7 +6,7 @@ template<typename T>
 class vector {
  public:
   vector() : _size(0), _data(nullptr) {};
-  vector(int size);
+  vector(size_t size);
   ~vector();
 
   inline __host__ __device__ size_t size() {
@@ -17,6 +17,18 @@ class vector {
   }
   inline __host__ __device__ T *data() {
       return _data;
+  }
+  inline __host__ __device__ T *begin() const {
+      return _data;
+  }
+  inline __host__ __device__ T *begin() {
+      return _data;
+  }
+  inline __host__ __device__ T *end() const {
+      return _data + _size - 1;
+  }
+  inline __host__ __device__ T *end() {
+      return _data + _size - 1;
   }
   inline __device__ const T &operator[](size_t idx) const {
       return _data[idx];
@@ -31,7 +43,7 @@ class vector {
 };
 
 template<typename T>
-inline vector<T>::vector(const int size) : vector() {
+inline vector<T>::vector(const size_t size) : vector() {
     _size = size;
     cudaMalloc(reinterpret_cast<void **>(_data), size * sizeof(T));
 }
