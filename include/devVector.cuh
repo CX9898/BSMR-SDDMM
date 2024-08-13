cf600889e44d4050d9a830b36c59b37bfd92fb9e
chengxing <zcx5613@gmail.com>
2024-08-13 14:01:10 +0900

    change value name and fixed cudaError illegal memory access was encountered

--- include/devVector.cuh
+++ include/devVector.cuh
@@ -1,55 +1,55 @@
 #pragma once
 
+#include <cuda_runtime.h>
+
 namespace dev {
 
 template<typename T>
 class vector {
  public:
-  vector() : _size(0), _data(nullptr) {};
+  vector() : size_(0), data_(nullptr) {};
   vector(size_t size);
-  ~vector();
+  ~vector() {
+      if (data_) { cudaFree(data_); }
+  };
 
   inline __host__ __device__ size_t size() {
-      return _size;
+      return size_;
   }
   inline __host__ __device__ const T *data() const {
-      return _data;
+      return data_;
   }
   inline __host__ __device__ T *data() {
-      return _data;
+      return data_;
   }
   inline __host__ __device__ T *begin() const {
-      return _data;
+      return data_;
   }
   inline __host__ __device__ T *begin() {
-      return _data;
+      return data_;
   }
   inline __host__ __device__ T *end() const {
-      return _data + _size - 1;
+      return data_ + size_ - 1;
   }
   inline __host__ __device__ T *end() {
-      return _data + _size - 1;
+      return data_ + size_ - 1;
   }
   inline __device__ const T &operator[](size_t idx) const {
-      return _data[idx];
+      return data_[idx];
   }
   inline __device__ T &operator[](size_t idx) {
-      return _data[idx];
+      return data_[idx];
   }
 
  private:
-  size_t _size;
-  T *_data;
+  size_t size_;
+  T *data_ = nullptr;
 };
 
 template<typename T>
 inline vector<T>::vector(const size_t size) : vector() {
-    _size = size;
-    cudaMalloc(reinterpret_cast<void **>(_data), size * sizeof(T));
+    size_ = size;
+    cudaMalloc(reinterpret_cast<void **> (data_), size * sizeof(T));
 }
 
-template<typename T>
-inline vector<T>::~vector() {
-    if (_data) { cudaFree(_data); }
-}
 } // namespace dev
