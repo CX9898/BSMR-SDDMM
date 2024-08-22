5d591bf48d19ac0727cd6af1591d2d38d07befbc
chengxing <zcx5613@gmail.com>
2024-08-22 15:21:31 +0900

    Add MATRIX TYPE

--- include/wmmaSetting.hpp
+++ include/wmmaSetting.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <cuda_fp16.h>
+
+using MATRIX_A_TYPE = __half;
+using MATRIX_B_TYPE = __half;
+using MATRIX_C_TYPE = float;
+
 // The dimensions currently supported by WMMA
 const int WMMA_M = 16;
 const int WMMA_N = 16;
