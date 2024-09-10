c9ab94cd458ade2d0d9b323504d6972fe79969e8
ZOUCHENGXING <zcx5613@gmail.com>
2024-09-10 17:24:14 +0900

    Add inclusive_scan()

--- include/cudaUtil.cuh
+++ include/cudaUtil.cuh
@@ -17,13 +17,13 @@ namespace host {
 void sort(uint64_t *first, uint64_t *last);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first);
-
+void inclusive_scan(size_t *first, size_t *last, size_t *result);
 } // namespace host
 
 namespace dev {
 
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first);
-
+void inclusive_scan(size_t *first, size_t *last, size_t *result);
 inline void cudaSync() {
     cudaDeviceSynchronize();
 }
--- src/cudaUtil.cu
+++ src/cudaUtil.cu
@@ -17,12 +17,18 @@ void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first)
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first) {
     thrust::sort_by_key(thrust::host, key_first, key_last, value_first);
 }
+void inclusive_scan(size_t *first, size_t *last, size_t *result) {
+    thrust::inclusive_scan(thrust::host, first, last, result);
+}
 } // namespace host
 
 namespace dev {
 
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first) {
     thrust::sort_by_key(thrust::device, key_first, key_last, value_first);
+}
+void inclusive_scan(size_t *first, size_t *last, size_t *result) {
+    thrust::inclusive_scan(thrust::device, first, last, result);
 }
 } // namespace dev
 
