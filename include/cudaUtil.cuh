8f455c994c528b44d815dd43c43cd6bb3870e1ea
ZOUCHENGXING <zcx5613@gmail.com>
2024-09-10 15:41:48 +0900

    Template functions change separated programming to inclusive programming

--- include/cudaUtil.cuh
+++ include/cudaUtil.cuh
@@ -12,47 +12,6 @@ inline void printCudaErrorStringSync() {
     fprintf(stderr, "CUDA Error : %s\n", cudaGetErrorString(cudaDeviceSynchronize()));
 }
 
-template<typename T>
-inline void H2D(T *dev, const T *host, const size_t size) {
-    cudaMemcpy(dev, host, size * sizeof(T), cudaMemcpyHostToDevice);
-}
-
-template<typename T>
-inline void H2D(T *dev, const std::vector<T> &host) {
-    cudaMemcpy(dev, host.data(), host.size() * sizeof(T), cudaMemcpyHostToDevice);
-}
-
-template<typename T>
-inline void D2H(T *host, const T *dev, const size_t size) {
-    cudaMemcpy(host, dev, size * sizeof(T), cudaMemcpyDeviceToHost);
-}
-
-template<typename T>
-inline void D2H(std::vector<T> &host, const T *dev, const size_t size) {
-    host.clear();
-    host.resize(size);
-    cudaMemcpy(host.data(), dev, size * sizeof(T), cudaMemcpyDeviceToHost);
-}
-
-template<typename T>
-inline std::vector<T> D2H(const T *dev, const size_t size) {
-    std::vector<T> host(size);
-    cudaMemcpy(host.data(), dev, size * sizeof(T), cudaMemcpyDeviceToHost);
-    return host;
-}
-
-template<typename T>
-inline void D2D(T *dest, const T *src, const size_t size) {
-    cudaMemcpy(dest, src, size * sizeof(T), cudaMemcpyDeviceToDevice);
-}
-
-template<typename T>
-inline std::vector<T> D2H(const dev::vector<T> &dev) {
-    std::vector<T> host(dev.size());
-    cudaMemcpy(host.data(), dev.data(), sizeof(T) * dev.size(), cudaMemcpyDeviceToHost);
-    return host;
-}
-
 namespace host {
 
 void sort(uint64_t *first, uint64_t *last);
--- include/devVector.cuh
+++ include/devVector.cuh
@@ -82,3 +82,45 @@ inline T *vector<T>::end() {
 }
 
 } // namespace dev
+
+
+template<typename T>
+inline void H2D(T *dev, const T *host, const size_t size) {
+    cudaMemcpy(dev, host, size * sizeof(T), cudaMemcpyHostToDevice);
+}
+
+template<typename T>
+inline void H2D(T *dev, const std::vector<T> &host) {
+    cudaMemcpy(dev, host.data(), host.size() * sizeof(T), cudaMemcpyHostToDevice);
+}
+
+template<typename T>
+inline void D2H(T *host, const T *dev, const size_t size) {
+    cudaMemcpy(host, dev, size * sizeof(T), cudaMemcpyDeviceToHost);
+}
+
+template<typename T>
+inline void D2H(std::vector<T> &host, const T *dev, const size_t size) {
+    host.clear();
+    host.resize(size);
+    cudaMemcpy(host.data(), dev, size * sizeof(T), cudaMemcpyDeviceToHost);
+}
+
+template<typename T>
+inline std::vector<T> D2H(const T *dev, const size_t size) {
+    std::vector<T> host(size);
+    cudaMemcpy(host.data(), dev, size * sizeof(T), cudaMemcpyDeviceToHost);
+    return host;
+}
+
+template<typename T>
+inline void D2D(T *dest, const T *src, const size_t size) {
+    cudaMemcpy(dest, src, size * sizeof(T), cudaMemcpyDeviceToDevice);
+}
+
+template<typename T>
+inline std::vector<T> D2H(const dev::vector<T> &dev) {
+    std::vector<T> host(dev.size());
+    cudaMemcpy(host.data(), dev.data(), sizeof(T) * dev.size(), cudaMemcpyDeviceToHost);
+    return host;
+}
