7e3b1a8893a9af90b24d8aac14d2ce40729174f1
chengxing <zcx5613@gmail.com>
2024-09-12 11:23:25 +0000

    Update cudaUtil

--- include/cudaUtil.cuh
+++ include/cudaUtil.cuh
@@ -7,13 +7,11 @@
 
 #include "devVector.cuh"
 
-namespace cuUtil {
 inline void printCudaErrorStringSync() {
     fprintf(stderr, "CUDA Error : %s\n", cudaGetErrorString(cudaDeviceSynchronize()));
 }
 
 namespace host {
-
 void sort(uint64_t *first, uint64_t *last);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first);
@@ -21,7 +19,8 @@ void inclusive_scan(size_t *first, size_t *last, size_t *result);
 } // namespace host
 
 namespace dev {
-
+void sort(uint64_t *first, uint64_t *last);
+void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first);
 void inclusive_scan(size_t *first, size_t *last, size_t *result);
 inline void cudaSync() {
@@ -29,5 +28,3 @@ inline void cudaSync() {
 }
 
 } // namespace dev
-
-}// namespace cuUtil
--- src/cudaUtil.cu
+++ src/cudaUtil.cu
@@ -2,8 +2,6 @@
 
 #include "cudaUtil.cuh"
 
-namespace cuUtil {
-
 namespace host {
 
 void sort(uint64_t *first, uint64_t *last) {
@@ -23,7 +21,12 @@ void inclusive_scan(size_t *first, size_t *last, size_t *result) {
 } // namespace host
 
 namespace dev {
-
+void sort(uint64_t *first, uint64_t *last) {
+    thrust::sort(thrust::device, first, last);
+}
+void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first) {
+    thrust::sort_by_key(thrust::device, key_first, key_last, value_first);
+}
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first) {
     thrust::sort_by_key(thrust::device, key_first, key_last, value_first);
 }
@@ -31,5 +34,3 @@ void inclusive_scan(size_t *first, size_t *last, size_t *result) {
     thrust::inclusive_scan(thrust::device, first, last, result);
 }
 } // namespace dev
-
-}// namespace cuUtil
