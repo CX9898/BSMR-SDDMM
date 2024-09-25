e746ef58041699a01f48366768ded30fb013502d
chengxing <zcx5613@gmail.com>
2024-09-25 09:14:14 +0000

    change function name

--- include/kernel.cuh
+++ include/kernel.cuh
@@ -14,21 +14,21 @@ __global__ void sddmm_gpu(const UIN M, const UIN N, const UIN K,
                           const float *matrixS,
                           float *matrixP);
 
-__global__ void sddmm_coo_gpu(class TensorCoreConfig tensorCoreConfig,
-                              const UIN M, const UIN N, const UIN K, const UIN nnz,
-                              const half *matrixA, const half *matrixB,
-                              const UIN *matrixSRowIndex,
-                              const UIN *matrixSColIndex,
-                              const UIN *matrixTileIndex,
-                              const float *matrixS,
-                              float *matrixP);
+__global__ void sddmm_gpu_coo_1(class TensorCoreConfig tensorCoreConfig,
+                                const UIN M, const UIN N, const UIN K, const UIN nnz,
+                                const half *matrixA, const half *matrixB,
+                                const UIN *matrixSRowIndex,
+                                const UIN *matrixSColIndex,
+                                const UIN *matrixTileIndex,
+                                const float *matrixS,
+                                float *matrixP);
 
-__global__ void sddmm_gpu_coo2(class TensorCoreConfig tensorCoreConfig,
-                               const UIN M, const UIN N, const UIN K, const UIN nnz,
-                               const half *matrixA, const half *matrixB,
-                               const UIN *matrixSRowIndex,
-                               const UIN *matrixSColIndex,
-                               const float *matrixS,
-                               const UIN *matrixSTileMappedToWarpIndex,
-                               const UIN *matrixSTileMappedToWarpIndexData,
-                               float *matrixP);
+__global__ void sddmm_gpu_coo_2(class TensorCoreConfig tensorCoreConfig,
+                                const UIN M, const UIN N, const UIN K, const UIN nnz,
+                                const half *matrixA, const half *matrixB,
+                                const UIN *matrixSRowIndex,
+                                const UIN *matrixSColIndex,
+                                const float *matrixS,
+                                const UIN *matrixSTileMappedToWarpIndex,
+                                const UIN *matrixSTileMappedToWarpIndexData,
+                                float *matrixP);
--- src/kernel.cu
+++ src/kernel.cu
@@ -272,14 +272,14 @@ __global__ void sddmm_gpu(const UIN M, const UIN N, const UIN K,
 
 }
 
-__global__ void sddmm_coo_gpu(class TensorCoreConfig tensorCoreConfig,
-                              const UIN M, const UIN N, const UIN K, const UIN nnz,
-                              const half *matrixA, const half *matrixB,
-                              const UIN *matrixSRowIndex,
-                              const UIN *matrixSColIndex,
-                              const UIN *matrixTileIndex,
-                              const float *matrixS,
-                              float *matrixP) {
+__global__ void sddmm_gpu_coo_1(class TensorCoreConfig tensorCoreConfig,
+                                const UIN M, const UIN N, const UIN K, const UIN nnz,
+                                const half *matrixA, const half *matrixB,
+                                const UIN *matrixSRowIndex,
+                                const UIN *matrixSColIndex,
+                                const UIN *matrixTileIndex,
+                                const float *matrixS,
+                                float *matrixP) {
     tensorCoreConfig.initByKernel(blockDim, blockIdx, threadIdx);
 
     const UIN pRowId = tensorCoreConfig.tileRow();
@@ -334,15 +334,15 @@ __global__ void sddmm_coo_gpu(class TensorCoreConfig tensorCoreConfig,
 
 }
 
-__global__ void sddmm_gpu_coo2(class TensorCoreConfig tensorCoreConfig,
-                               const UIN M, const UIN N, const UIN K, const UIN nnz,
-                               const half *matrixA, const half *matrixB,
-                               const UIN *matrixSRowIndex,
-                               const UIN *matrixSColIndex,
-                               const float *matrixS,
-                               const UIN *matrixSTileMappedToWarpIndex,
-                               const UIN *matrixSTileMappedToWarpIndexData,
-                               float *matrixP) {
+__global__ void sddmm_gpu_coo_2(class TensorCoreConfig tensorCoreConfig,
+                                const UIN M, const UIN N, const UIN K, const UIN nnz,
+                                const half *matrixA, const half *matrixB,
+                                const UIN *matrixSRowIndex,
+                                const UIN *matrixSColIndex,
+                                const float *matrixS,
+                                const UIN *matrixSTileMappedToWarpIndex,
+                                const UIN *matrixSTileMappedToWarpIndexData,
+                                float *matrixP) {
     tensorCoreConfig.initByKernel(blockDim, blockIdx, threadIdx);
 
     const UIN pRowId = tensorCoreConfig.tileRow();
