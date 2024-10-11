6a0a29ce6b9829f38d6032b434ea786a1eb96228
chengxing <zcx5613@gmail.com>
2024-10-11 08:37:19 +0000

    Update autoTest.sh

--- autoTestTool/autoTest.sh
+++ autoTestTool/autoTest.sh
@@ -24,20 +24,20 @@ autoTest(){
     echo "* Start test..."
 
     # 使用 find 命令读取目录中的所有文件名，并存储到数组中
-    filesList=($(find "$@" -maxdepth 1 -type f -printf '%f\n'))
+    filesList=($(find "${test_file_folder}" -maxdepth 1 -type f -printf '%f\n'))
 
-    for file in "${filesList[@]}"; do
-          echo "* ${test_file_folder}$file start testing..."
-          ${program_path}${program_name} ${test_file_folder}${file} >> ${test_log_file_name}
-          echo "* ${test_file_folder}$file end test"
-    done
+#    for file in "${filesList[@]}"; do
+#          echo "* ${test_file_folder}$file start testing..."
+#          ${program_path}${program_name} ${test_file_folder}${file} >> ${test_log_file_name}
+#          echo "* ${test_file_folder}$file end test"
+#    done
 
     echo "* End test"
     echo "* Test information file: $(pwd)${test_log_file_name}"
 }
 
-autoTest "$(pwd)/../dataset/test"
+autoTest
 
 g++ autoAnalysisResults.cpp -o autoAnalysisResults
 
-autoAnalysisResults $(pwd)${test_log_file_name}
+"$(pwd)/autoAnalysisResults" "$(pwd)/${test_log_file_name}"
