c1eb7176fb183a3680ff279df58768fe88a333f7
chengxing <zcx5613@gmail.com>
2024-10-16 06:27:11 +0000

    Update autoTestTool

--- autoTestTool/autoTestTool.sh
+++ autoTestTool/autoTestTool.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+script_file_path="$(dirname "$0")/"
+
 # 测试文件路径
-test_file_folder="$(pwd)/../dataset/matrix_5000_5000_/"
+test_file_folder_path="${script_file_path}../dataset/matrix_10000_10000_/"
 
 # 设置多个K
 K=(256 500 1000 2000 3000 4000 5000)
@@ -11,14 +13,14 @@ for element in "${K[@]}"; do
 done
 echo
 
-zcx_build_folder_name="build_zcx"
-zcx_cmake_file_path="$(pwd)/../"
-zcx_program_path="$(pwd)/${zcx_build_folder_name}/"
+zcx_build_folder_path="${script_file_path}build_zcx/"
+zcx_cmake_file_path="${script_file_path}../"
+zcx_program_path="${zcx_build_folder_path}"
 zcx_program_name="sddmm-gpu"
 
-isratnisa_build_folder_name="build_isratnisa"
-isratnisa_cmake_file_path="$(pwd)/../isratnisa/HiPC18"
-isratnisa_program_path="$(pwd)/${isratnisa_build_folder_name}/"
+isratnisa_build_folder_path="${script_file_path}build_isratnisa/"
+isratnisa_cmake_file_path="${script_file_path}../isratnisa/HiPC18/"
+isratnisa_program_path="${isratnisa_build_folder_path}"
 isratnisa_program_name="isratnisa-sddmm"
 
 # 日志文件名
@@ -28,23 +30,26 @@ isratnisa_test_log_filename="isratnisaTestLog"
 # 分析结果日志文件名
 analysis_results_log_filename="analysisResultsLog"
 
-auto_analysis_results_source_filename="autoAnalysisResults.cpp"
-auto_analysis_results_program_name="autoAnalysisResults"
+auto_analysis_results_source_filename="${script_file_path}autoAnalysisResults.cpp"
+auto_analysis_results_program="${script_file_path}autoAnalysisResults"
 
 # 参数1: 构建地址
 # 参数2: "CMakeLists.txt"文件路径
 build_program(){
+  local build_path=${1}
+  local cmake_file_path=${2}
+
   echo "* Building the program..."
-  if [ ! -e ${1} ]; then
-      mkdir ${1}
+  if [ ! -e ${build_path} ]; then
+      mkdir ${build_path}
   fi
-  cmake -S ${2} -B ${1} > /dev/null
-  cmake --build ${1} > /dev/null
-  echo "* Build complete : $(pwd)/${1}"
+  cmake -S ${cmake_file_path} -B ${build_path} > /dev/null
+  cmake --build ${build_path} > /dev/null
+  echo "* Build complete : ${script_file_path}${build_path}"
 }
 
-build_program ${zcx_build_folder_name} ${zcx_cmake_file_path}
-build_program ${isratnisa_build_folder_name} ${isratnisa_cmake_file_path}
+build_program ${zcx_build_folder_path} ${zcx_cmake_file_path}
+build_program ${isratnisa_build_folder_path} ${isratnisa_cmake_file_path}
 
 # 创建不重名的日志文件, 并且将日志文件名更新在全局变量`log_filename`中
 # 参数1 : 原始日志文件名
@@ -53,15 +58,15 @@ create_log_file(){
   local log_filename=$1
 
   # 避免有同名文件
-  if [ -e "${log_filename}${log_file_suffix}" ]; then
+  if [ -e "${script_file_path}${log_filename}${log_file_suffix}" ]; then
     local file_id=1
-    while [ -e "${log_filename}${file_id}${log_file_suffix}" ]
+    while [ -e "${script_file_path}${log_filename}${file_id}${log_file_suffix}" ]
     do
       ((file_id++))
     done
     log_filename="${log_filename}${file_id}"
   fi
-  log_file=${log_filename}${log_file_suffix}
+  log_file=${script_file_path}${log_filename}${log_file_suffix}
   echo "* Create file: ${log_file}"
   touch ${log_file}
 }
@@ -78,7 +83,7 @@ analysis_results_log_file=${log_file}
 # 参数2 : 测试日志文件
 autoTest(){
   # 使用 find 命令读取目录中的所有文件名，并存储到数组中
-  local filesList=($(find "${test_file_folder}" -maxdepth 1 -type f -printf '%f\n'))
+  local filesList=($(find "${test_file_folder_path}" -maxdepth 1 -type f -printf '%f\n'))
 
   local numTestFiles=${#filesList[@]}
   echo "* Number of test files: = ${numTestFiles}"
@@ -93,13 +98,13 @@ autoTest(){
 
   local file_id=1
   for file in "${filesList[@]}"; do
-    echo -e "\t * file_id : ${file_id} ${test_file_folder}$file start testing..."
+    echo -e "\t * file_id : ${file_id} ${test_file_folder_path}$file start testing..."
     local k_id=1
 
     for k in "${K[@]}"; do
       echo -e "\t\t * k_id : ${k_id} K = ${k} start testing..."
       echo -e "\n---new data---\n" >> ${2}
-      $1 ${test_file_folder}${file} ${k} 192 50000 >> ${2}
+      $1 ${test_file_folder_path}${file} ${k} 192 50000 >> ${2}
       ((k_id++))
     done
 
@@ -107,15 +112,15 @@ autoTest(){
   done
 
   echo "* End test"
-  echo "* Test information file: $(pwd)/${log_file}"
+  echo "* Test information file: ${script_file_path}${log_file}"
 }
 
 autoTest ${zcx_program_path}${zcx_program_name} ${zcx_test_log_file}
 autoTest ${isratnisa_program_path}${isratnisa_program_name} ${isratnisa_test_log_file}
 
 # 编译分析结果程序
-g++ ${auto_analysis_results_source_filename} -o ${auto_analysis_results_program_name}
+g++ ${auto_analysis_results_source_filename} -o ${auto_analysis_results_program}
 
-echo "Start analyzing results..."
-"$(pwd)/${auto_analysis_results_program_name}" ${zcx_test_log_file} ${isratnisa_test_log_file} >> ${analysis_results_log_file}
-echo "Results analysis completed: ${analysis_results_log_file}"
+echo "* Start analyzing results..."
+${auto_analysis_results_program} ${zcx_test_log_file} ${isratnisa_test_log_file} >> ${analysis_results_log_file}
+echo "* Results analysis completed: ${analysis_results_log_file}"
