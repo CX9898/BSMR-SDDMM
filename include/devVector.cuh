417d54126fc299f46c60922277ca8db126e2dceb
ZOUCHENGXING <zcx5613@gmail.com>
2024-10-28 23:25:08 +0900

    Update devVector.cuh

--- include/devVector.cuh
+++ include/devVector.cuh
@@ -54,16 +54,16 @@ template<typename T>
 inline vector<T>::vector(const size_t size) : vector() {
     size_ = size;
     cudaMalloc(reinterpret_cast<void **> (&data_), size * sizeof(T));
-    if(!data_){
-        fprintf(stderr,"Device out of memory, memory allocation failed\n");
+    if (!data_) {
+        fprintf(stderr, "Device out of memory, memory allocation failed\n");
     }
 }
 template<typename T>
 inline vector<T>::vector(const vector<T> &src) {
     size_ = src.size_;
     cudaMalloc(reinterpret_cast<void **> (&data_), src.size_ * sizeof(T));
-    if(!data_){
-        fprintf(stderr,"Device out of memory, memory allocation failed\n");
+    if (!data_) {
+        fprintf(stderr, "Device out of memory, memory allocation failed\n");
     }
     cudaMemcpy(data_, src.data_, size_ * sizeof(T), cudaMemcpyDeviceToDevice);
 }
@@ -72,8 +72,8 @@ template<typename T>
 inline vector<T>::vector(const std::vector<T> &src) {
     size_ = src.size();
     cudaMalloc(reinterpret_cast<void **> (&data_), src.size() * sizeof(T));
-    if(!data_){
-        fprintf(stderr,"Device out of memory, memory allocation failed\n");
+    if (!data_) {
+        fprintf(stderr, "Device out of memory, memory allocation failed\n");
     }
     cudaMemcpy(data_, src.data(), src.size() * sizeof(T), cudaMemcpyHostToDevice);
 }
@@ -85,8 +85,8 @@ inline void vector<T>::resize(size_t size) {
         cudaFree(data_);
     }
     cudaMalloc(reinterpret_cast<void **> (&data_), size * sizeof(T));
-    if(!data_){
-        fprintf(stderr,"Device out of memory, memory allocation failed\n");
+    if (!data_) {
+        fprintf(stderr, "Device out of memory, memory allocation failed\n");
     }
 }
 
@@ -189,7 +189,6 @@ inline std::vector<T> d2h(const dev::vector<T> &dev) {
     return host;
 }
 
-
 template<typename T>
 inline void d2d(T *dest, const T *src, const size_t size) {
     cudaMemcpy(dest, src, size * sizeof(T), cudaMemcpyDeviceToDevice);
@@ -201,3 +200,32 @@ inline void d2d(dev::vector<T> &dest, const dev::vector<T> &src) {
     dest.resize(src.size());
     cudaMemcpy(dest.data(), src.data(), src.size() * sizeof(T), cudaMemcpyDeviceToDevice);
 }
+
+namespace pxy {
+template<typename T>
+class vector {
+ public:
+  __host__ __device__ vector(const dev::vector<T> &devVec) : size_(devVec.size()), data_((T *) devVec.data()) {}
+
+  __host__ __device__ size_t size() const {
+      return size_;
+  }
+  __host__ __device__ const T *data() const {
+      return data_;
+  }
+  __host__ __device__ T *data() {
+      return data_;
+  }
+  __host__ __device__ const T &operator[](size_t idx) const {
+      return data_[idx];
+  }
+  __host__ __device__ T &operator[](size_t idx) {
+      return data_[idx];
+  }
+
+ private:
+  size_t size_;
+  T *data_ = nullptr;
+
+};
+} // namespace pxy
