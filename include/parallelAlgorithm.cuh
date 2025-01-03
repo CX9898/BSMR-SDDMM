0f99a08811d969259f25bf15095770584cdef595
ZOU CHENGXING <zcx5613@gmail.com>
2025-01-03 16:58:32 +0900

    update

--- include/parallelAlgorithm.cuh
+++ include/parallelAlgorithm.cuh
@@ -19,8 +19,23 @@ void fill_n(uint32_t *first, size_t size, uint32_t val);
 void sort(uint32_t *first, uint32_t *last);
 void sort(uint64_t *first, uint64_t *last);
 void sort_by_key(uint32_t *key_first, uint32_t *key_last, uint32_t *value_first);
+void sort_by_key(uint32_t *key_first, uint32_t *key_last, int *value_first);
+void sort_by_key(uint32_t *key_first, uint32_t *key_last, float *value_first);
+void sort_by_key(uint32_t *key_first, uint32_t *key_last, double *value_first);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first);
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first);
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      int *value2_first);
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      float *value2_first);
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      double *value2_first);
 void inclusive_scan(size_t *first, size_t *last, size_t *result);
 void inclusive_scan(uint32_t *first, uint32_t *last, uint32_t *result);
 } // namespace host
--- src/parallelAlgorithm.cu
+++ src/parallelAlgorithm.cu
@@ -1,4 +1,6 @@
 #include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+#include <thrust/iterator/zip_iterator.h>
 
 #include "parallelAlgorithm.cuh"
 
@@ -18,8 +20,57 @@ void sort_by_key(uint64_t *key_first, uint64_t *key_last, uint64_t *value_first)
 void sort_by_key(uint32_t *key_first, uint32_t *key_last, uint32_t *value_first) {
     thrust::sort_by_key(thrust::host, key_first, key_last, value_first);
 }
+void sort_by_key(uint32_t *key_first, uint32_t *key_last, int *value_first) {
+    thrust::sort_by_key(thrust::host, key_first, key_last, value_first);
+}
+void sort_by_key(uint32_t *key_first, uint32_t *key_last, float *value_first) {
+    thrust::sort_by_key(thrust::host, key_first, key_last, value_first);
+}
+void sort_by_key(uint32_t *key_first, uint32_t *key_last, double *value_first) {
+    thrust::sort_by_key(thrust::host, key_first, key_last, value_first);
+}
 void sort_by_key(uint64_t *key_first, uint64_t *key_last, float *value_first) {
     thrust::sort_by_key(thrust::host, key_first, key_last, value_first);
+}
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      uint32_t *value2_first) {
+    thrust::sort_by_key(thrust::host,
+                        key_first,
+                        key_last,
+                        thrust::make_zip_iterator(thrust::make_tuple(value1_first, value2_first)));
+
+}
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      int *value2_first) {
+    thrust::sort_by_key(thrust::host,
+                        key_first,
+                        key_last,
+                        thrust::make_zip_iterator(thrust::make_tuple(value1_first, value2_first)));
+
+}
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      float *value2_first) {
+    thrust::sort_by_key(thrust::host,
+                        key_first,
+                        key_last,
+                        thrust::make_zip_iterator(thrust::make_tuple(value1_first, value2_first)));
+
+}
+void sort_by_key_for_multiple_vectors(uint32_t *key_first,
+                                      uint32_t *key_last,
+                                      uint32_t *value1_first,
+                                      double *value2_first) {
+    thrust::sort_by_key(thrust::host,
+                        key_first,
+                        key_last,
+                        thrust::make_zip_iterator(thrust::make_tuple(value1_first, value2_first)));
+
 }
 void inclusive_scan(size_t *first, size_t *last, size_t *result) {
     thrust::inclusive_scan(thrust::host, first, last, result);
