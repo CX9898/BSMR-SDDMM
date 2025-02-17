508a6824874101f45fc3fd73bc1312cb718f3d31
chengxing <zcx5613@gmail.com>
2025-02-17 17:36:38 +0900

    2025-2-17 commit

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -7,79 +7,79 @@
 #include "Matrix.hpp"
 
 class Logger {
-public:
-    Logger() {
+ public:
+  Logger() {
 #ifdef NDEBUG
-        buildType_ = "Release";
+      buildType_ = "Release";
 #endif
 
 #ifndef NDEBUG
-        buildType_ = "Debug";
+      buildType_ = "Debug";
 #endif
 
-        cudaDeviceProp deviceProp{};
-        cudaGetDeviceProperties(&deviceProp, 0);
-        gpu_ =  deviceProp.name;
+      cudaDeviceProp deviceProp{};
+      cudaGetDeviceProperties(&deviceProp, 0);
+      gpu_ = deviceProp.name;
 
-        matrixA_type_ = typeid(MATRIX_A_TYPE).name();
-        matrixB_type_ = typeid(MATRIX_B_TYPE).name();
-        matrixC_type_ = typeid(MATRIX_C_TYPE).name();
+      matrixA_type_ = typeid(MATRIX_A_TYPE).name();
+      matrixB_type_ = typeid(MATRIX_B_TYPE).name();
+      matrixC_type_ = typeid(MATRIX_C_TYPE).name();
 
-        wmma_m_ = WMMA_M;
-        wmma_n_ = WMMA_N;
-        wmma_k_ = WMMA_K;
-    };
+      wmma_m_ = WMMA_M;
+      wmma_n_ = WMMA_N;
+      wmma_k_ = WMMA_K;
+  };
 
-    template<typename T>
-    inline void getInformation(const sparseMatrix::COO<T> &matrix);
+  template<typename T>
+  inline void getInformation(const sparseMatrix::COO<T> &matrix);
 
-    template<typename T>
-    inline void getInformation(const Matrix<T> &matrixA, const Matrix<T> &matrixB){
-        K_ = matrixA.col();
-        matrixA_storageOrder_ = matrixA.storageOrder() == MatrixStorageOrder::row_major ? "row_major" : "col_major";
-        matrixB_storageOrder_ = matrixB.storageOrder() == MatrixStorageOrder::row_major ? "row_major" : "col_major";
+  template<typename T>
+  inline void getInformation(const Matrix<T> &matrixA, const Matrix<T> &matrixB) {
+      K_ = matrixA.col();
+      matrixA_storageOrder_ = matrixA.storageOrder() == MatrixStorageOrder::row_major ? "row_major" : "col_major";
+      matrixB_storageOrder_ = matrixB.storageOrder() == MatrixStorageOrder::row_major ? "row_major" : "col_major";
 
-    }
+  }
 
-    inline float &zcx_sddmm(){ return zcx_sddmm_;}
-    inline float &zcx_other(){ return zcx_other_;}
-    inline float &cuSparse(){ return cuSparse_;}
+  inline float &zcx_sddmm() { return zcx_sddmm_; }
+  inline float &zcx_other() { return zcx_other_; }
+  inline float &cuSparse() { return cuSparse_; }
 
-    inline void printLogInformation();
+  inline void printLogInformation();
 
-private:
-    std::string checkData_;
-    size_t numError;
+ private:
+  std::string checkData_;
+  size_t numError;
 
-    std::string gpu_;
-    std::string buildType_;
+  std::string gpu_;
+  std::string buildType_;
 
-    size_t wmma_m_;
-    size_t wmma_n_;
-    size_t wmma_k_;
+  size_t wmma_m_;
+  size_t wmma_n_;
+  size_t wmma_k_;
 
-    std::string matrixA_type_;
-    std::string matrixB_type_;
-    std::string matrixC_type_;
+  std::string matrixA_type_;
+  std::string matrixB_type_;
+  std::string matrixC_type_;
 
-    std::string matrixA_storageOrder_;
-    std::string matrixB_storageOrder_;
+  std::string matrixA_storageOrder_;
+  std::string matrixB_storageOrder_;
 
-    size_t M_;
-    size_t N_;
-    size_t K_;
-    size_t NNZ_;
-    float sparsity_;
+  size_t M_;
+  size_t N_;
+  size_t K_;
+  size_t NNZ_;
+  float sparsity_;
 
-    float isratnisa_sddmm_;
-    float zcx_sddmm_;
+  float isratnisa_sddmm_;
+  float zcx_sddmm_;
 
-    float isratnisa_other_;
-    float zcx_other_;
+  float isratnisa_other_;
+  float zcx_other_;
 
-    float isratnisa_;
-    float zcx_;
-    float cuSparse_;
+  float isratnisa_;
+  float zcx_;
+  float cuSparse_;
 };
 
 template<typename T>
@@ -104,6 +104,9 @@ void Logger::printLogInformation() {
     printf("[matrixA type : %s]\n", matrixA_type_.c_str());
     printf("[matrixB type : %s]\n", matrixB_type_.c_str());
     printf("[matrixC type : %s]\n", matrixC_type_.c_str());
+
+    printf("[matrixA storageOrder : %s]\n", matrixA_storageOrder_.c_str());
+    printf("[matrixB storageOrder : %s]\n]", matrixB_storageOrder_.c_str());
 
     printf("[cuSparse : %.2f]\n", cuSparse_);
 
