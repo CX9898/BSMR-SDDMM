f2fc3c2865ef8cb9be6feaf9f29394a4175dbe20
chengxing <zcx5613@gmail.com>
2025-02-21 17:27:53 +0900

    Update

--- include/cudaUtil.cuh
+++ include/cudaUtil.cuh
@@ -4,7 +4,32 @@
 #include <cuda_runtime.h>
 
 namespace cuUtil {
+
+/**
+ * @funcitonName: printCudaErrorStringSync
+ * @functionInterpretation: Print the error message of the cuda runtime API, and synchronize the device
+ **/
 inline void printCudaErrorStringSync() {
     printf("cuda error : %s\n", cudaGetErrorString(cudaDeviceSynchronize()));
+}
+
+/**
+ * @funcitonName: calculateOccupancyMaxPotentialBlockSize
+ * @functionInterpretation: Calculate the optimal block size of the kernel function
+ * @input:
+ *  `func` : Kernel function
+ * @output:
+ * return a pair of int, the first element is the optimal block size, the second element is the minimal grid size
+ **/
+std::pair<int, int> calculateOccupancyMaxPotentialBlockSize(void *func) {
+    int minGridSize;
+    int blockSize;
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize,
+                                       &blockSize,
+                                       func,
+                                       0,
+                                       0);
+    printf("minGridSize: %d, blockSize: %d\n", minGridSize, blockSize);
+    return std::make_pair(blockSize, minGridSize);
 }
 } // namespace cuUtil
