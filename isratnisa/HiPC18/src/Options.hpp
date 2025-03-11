cb3b5e58075c963bf8aba42ce7a8c47114306a7e
chengxing <zcx5613@gmail.com>
2025-03-11 16:14:53 +0900

    Update

--- isratnisa/HiPC18/src/Options.hpp
+++ isratnisa/HiPC18/src/Options.hpp
@@ -5,11 +5,8 @@
 #include <vector>
 #include <unordered_set>
 
-const std::string folderPath("../dataset/test/matrix_20000_20000_/");
-//const std::string folderPath("./");
-//const std::string fileName = ("nips");
-//const std::string fileName = ("test2");
-const std::string fileName("matrix_20000_20000_20000000");
+const std::string folderPath("../../dataset/");
+const std::string fileName = ("nips");
 const std::string fileFormat(".mtx");
 const std::string filePath = folderPath + fileName + fileFormat;
 
