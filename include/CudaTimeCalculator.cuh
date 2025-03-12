07bbaec654e9dc3d3deb340fbfaec343ccff09bd
chengxing <zcx5613@gmail.com>
2025-03-12 18:58:46 +0900

    Update

--- include/CudaTimeCalculator.cuh
+++ include/CudaTimeCalculator.cuh
@@ -15,40 +15,39 @@ class CudaTimeCalculator {
  public:
   inline CudaTimeCalculator() {
       time_ = 0.0f;
-
-      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&star_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&start_));
       cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&stop_));
   }
 
   ~CudaTimeCalculator() {
-      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(star_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(start_));
       cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(stop_));
   }
 
-  inline void reset(){
-      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(star_));
-      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(stop_));
-
-      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&star_));
-      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&stop_));
+  // 复位计时器(不销毁事件)
+  inline void reset() {
+      time_ = 0.0f;
   }
 
-  inline void startClock() {
-      cudaTimeCalculator::cudaErrCheck_(cudaEventRecord(star_));
+  // 记录起始时间(可选 Stream)
+  inline void startClock(cudaStream_t stream = 0) {
+      cudaTimeCalculator::cudaErrCheck_(cudaEventRecord(start_, stream));
   }
 
-  inline void endClock() {
-      cudaTimeCalculator::cudaErrCheck_(cudaEventRecord(stop_));
-      cudaTimeCalculator::cudaErrCheck_(cudaEventSynchronize(stop_));
+  // 记录结束时间(可选 Stream)
+  inline void endClock(cudaStream_t stream = 0) {
+      cudaTimeCalculator::cudaErrCheck_(cudaEventRecord(stop_, stream));
   }
 
+  // 获取 GPU 执行时间(毫秒)
   inline float getTime() {
-      cudaTimeCalculator::cudaErrCheck_(cudaEventElapsedTime(&time_, star_, stop_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventSynchronize(stop_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventElapsedTime(&time_, start_, stop_));
       return time_;
   }
 
  private:
-  cudaEvent_t star_;
+  cudaEvent_t start_;
   cudaEvent_t stop_;
 
   float time_;
