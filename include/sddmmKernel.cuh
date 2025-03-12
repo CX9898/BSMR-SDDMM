18635e1092dae235a0c23a025c1ad44d4f96d204
chengxing <zcx5613@gmail.com>
2025-03-12 19:26:04 +0900

    2025-3-12 commit

--- include/sddmmKernel.cuh
+++ include/sddmmKernel.cuh
@@ -25,12 +25,3 @@ void sddmm_gpu_rebell(const Matrix<float> &matrixA,
                       const ReBELL &rebell,
                       sparseMatrix::CSR<float> &matrixP,
                       Logger &logger);
-
-// 在外部进行K迭代
-void sddmm_gpu_rebell_out_kIter(const Matrix<float> &matrixA,
-                                const Matrix<float> &matrixB,
-                                const float alpha, const float beta,
-                                const sparseMatrix::CSR<float> &matrixS,
-                                const ReBELL &rebell,
-                                sparseMatrix::CSR<float> &matrixP,
-                                float &time);
