4afe36b53e191047296041db58b08d82a2801ffc
chengxing <zcx5613@gmail.com>
2025-03-18 20:29:18 +0900

    Update autoTestTool

---
+++ autoTestTool/testing_the_dataset_of_isratnisa_paper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+results_path="dataset_of_isratnisa_paper_results/"
+dataset_path="${results_path}dataset/"
+
+# 下载数据集并解压, 并移动到指定目录, 并记录文件名
+file_list=""
+download_decompressing_move(){
+  local url=$1
+  local compressed_file=$(basename "$url")
+  wget -O ${compressed_file} ${url}
+  gunzip -f $compressed_file
+  local file=$(basename "${compressed_file}" .gz)
+  mkdir -p ${dataset_path}
+  mv ${file} ${dataset_path}
+  file_list="${file_list} ${dataset_path}${file}"
+}
+
+#############################################################
+# main
+
+mkdir -p ${results_path}
+
+download_decompressing_move "https://snap.stanford.edu/data/web-BerkStan.txt.gz"
+download_decompressing_move "https://snap.stanford.edu/data/web-Google.txt.gz"
+download_decompressing_move "https://snap.stanford.edu/data/web-NotreDame.txt.gz"
+
+# 生成矩阵文件列表
+matrix_list_file="${results_path}matrix_file_list.txt"
+> "${matrix_list_file}"
+find ${dataset_path} -type f > ${matrix_list_file}
+
+bash build_program.sh
+program_zcx="./build_zcx/sddmm-gpu"
+program_isratnisa="./build_isratnisa/isratnisa-sddmm"
+
+bash testTool.sh -f ${matrix_list_file} -p ${program_zcx} -n "${results_path}zcx_results"
+bash testTool.sh -f ${matrix_list_file} -p ${program_isratnisa} -n "${results_path}isratnisa_results"
+
+# 分析结果
+g++ autoAnalysisResults.cpp -o autoAnalysisResults
+./autoAnalysisResults zcx_results.log isratnisa_results.log > analysisResults.log
+echo "Results analysis completed: analysisResults.log"
