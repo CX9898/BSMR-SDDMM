b11cdd81a2b402c888a904d33fe82033b33c8786
chengxing <zcx5613@gmail.com>
2025-04-03 01:32:59 +0900

    Update testing_the_dataset_of_Sogang_University.sh

--- autoTestTool/testing_the_dataset_of_Sogang_University.sh
+++ autoTestTool/testing_the_dataset_of_Sogang_University.sh
@@ -17,20 +17,27 @@ mv -n ${results_path}2048_512_dlmc_data.txt dataset/dlmc/
 
 bash build_program.sh
 
+# 编译分析程序
+g++ autoAnalysisResults.cpp -o autoAnalysisResults
+
+# 测试 rn50 dataset
 bash testTool.sh -f ${results_path}dlmc/rn50_matrices.txt -p ./build_zcx/sddmm-gpu -n ${rn50_results_path}zcx_results_rn50
 bash testTool.sh -f ${results_path}dlmc/transformer_matrices.txt -p ./build_zcx/sddmm-gpu -n ${transformer_results_path}zcx_results_transformer
 
+# 分析结果
+./autoAnalysisResults "${rn50_results_path}zcx_results_rn50.log" "${rn50_results_path}isratnisa_results_rn50.log" > ${rn50_results_path}analysisResults_rn50.log
+echo "Results analysis completed: ${rn50_results_path}analysisResults_rn50.log"
+
+# 结果可视化
+python3 resultsVisualization.py -file ${rn50_results_path}analysisResults_rn50.log -outdir ${rn50_results_path}
+
+# 测试 transformer dataset
 bash testTool.sh -f ${results_path}dlmc/rn50_matrices.txt -p ./build_isratnisa/isratnisa-sddmm -n ${rn50_results_path}isratnisa_results_rn50
 bash testTool.sh -f ${results_path}dlmc/transformer_matrices.txt -p ./build_isratnisa/isratnisa-sddmm -n ${transformer_results_path}isratnisa_results_transformer
 
 # 分析结果
-g++ autoAnalysisResults.cpp -o autoAnalysisResults
-./autoAnalysisResults "${rn50_results_path}zcx_results_rn50.log" "${rn50_results_path}isratnisa_results_rn50.log" > ${rn50_results_path}analysisResults_rn50.log
-echo "Results analysis completed: ${rn50_results_path}analysisResults_rn50.log"
-
 ./autoAnalysisResults "${transformer_results_path}zcx_results_transformer.log" "${transformer_results_path}isratnisa_results_transformer.log" > ${transformer_results_path}analysisResults_transformer.log
 echo "Results analysis completed: ${transformer_results_path}analysisResults_transformer.log"
 
 # 结果可视化
-python3 resultsVisualization.py -file ${rn50_results_path}analysisResults_rn50.log -outdir ${rn50_results_path}
 python3 resultsVisualization.py -file ${transformer_results_path}analysisResults_transformer.log -outdir ${transformer_results_path}
