6f500112ae9e4379d82e70c51378de2830fd4781
chengxing <zcx5613@gmail.com>
2025-05-22 16:33:04 +0900

    Update testing_the_dataset_of_ASpT_paper.sh

--- autoTestTool/testing_the_dataset_of_ASpT_paper.sh
+++ autoTestTool/testing_the_dataset_of_ASpT_paper.sh
@@ -16,13 +16,13 @@ program_zcx="./build_zcx/sddmm-gpu"
 program_isratnisa="./build_isratnisa/isratnisa-sddmm"
 
 # 运行测试程序
-#bash testTool.sh -f ${matrix_list_file} -p ${program_zcx} -n "${results_path}zcx_results"
+bash testTool.sh -f ${matrix_list_file} -p ${program_zcx} -n "${results_path}zcx_results"
 bash testTool.sh -f ${matrix_list_file} -p ${program_isratnisa} -n "${results_path}isratnisa_results"
-#
-## 分析结果
-#g++ autoAnalysisResults.cpp -o autoAnalysisResults
-#./autoAnalysisResults "${results_path}zcx_results.log" "${results_path}isratnisa_results.log" > ${results_path}analysisResults.log
-#echo "Results analysis completed: ${results_path}analysisResults.log"
-#
-## 结果可视化
-#python3 resultsVisualization.py -file ${results_path}analysisResults.log -outdir ${results_path}
+
+# 分析结果
+g++ autoAnalysisResults.cpp -o autoAnalysisResults
+./autoAnalysisResults "${results_path}zcx_results.log" "${results_path}isratnisa_results.log" > ${results_path}analysisResults.log
+echo "Results analysis completed: ${results_path}analysisResults.log"
+
+# 结果可视化
+python3 resultsVisualization.py -file ${results_path}analysisResults.log -outdir ${results_path}
