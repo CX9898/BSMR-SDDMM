22d52674f30e5c300d676ba45574fd5ccccdd20d
chengxing <zcx5613@gmail.com>
2025-05-22 16:16:20 +0900

    Update make_matrices_list.sh

--- autoTestTool/make_matrices_list.sh
+++ autoTestTool/make_matrices_list.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 解析参数
-target_folder="results"
-while getopts "f:p:n:" opt; do
-    case ${opt} in
-        n) target_folder="$OPTARG" ;;  # 处理 -n 选项(目标文件夹)
-        ?) echo "用法: $0 -n <目标文件夹>"
-           exit 1 ;;
-    esac
-done
+#
+# Script Name: make_matrices_list.sh
+# Description: 创建一个包含所有矩阵文件路径的列表文件. 该脚本会遍历指定目录下的所有 .mtx 和 .smtx 文件, 并将它们的相对路径保存到一个文本文件中.
+# Usage: ./make_matrices_list.sh [目标文件夹]
+
+##############################################################################################
+
+# 目标文件夹
+target_folder=$1
 
 # 移除 target_folder 末尾的斜杠(/)
 target_folder="${target_folder%/}"
